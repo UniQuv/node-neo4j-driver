06bc0063d191dab44d61588cc1258905dbd3043f
UniQuv <34121768+UniQuv@users.noreply.github.com>
2018-05-07 18:21:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const neo4j = require("./src/Driver");
+const neo4j = require("./Driver");
 
-let neo = new neo4j("http://localhost:7474/", "neo4j", "zhangbaili");
+let neo = new neo4j("http://localhost:7474/", "neo4j", "neo4j");
 
 neo.runCyphers(["CREATE (n:bog) RETURN n"], (err, nodes, rels) => {
         if(err){
